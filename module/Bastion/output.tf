f72400704fa6bf43c3753bf3e71738fc2dfbe664
mistafour <edokens44@gmail.com>
2026-07-26 13:18:03 +0100

    Created Bastion server

---
+++ module/Bastion/output.tf
@@ -0,0 +1,18 @@
+output "bastion_sg" {
+  value = aws_security_group.bastion-security-group.id
+}
+data "aws_instances" "bastion_instances" {
+  filter {
+    name   = "tag:Name"
+    values = ["${var.name}-bastion-asg"]
+  }
+  filter {
+    name   = "instance-state-name"
+    values = ["running"]
+  }
+  depends_on = [aws_autoscaling_group.bastion-asg]
+}
+output "bastion_public_ip" {
+  value       = data.aws_instances.bastion_instances.public_ips[0]
+  description = "The public IP address of the bastion instance"
+}
